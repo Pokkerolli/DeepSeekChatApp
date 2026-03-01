a333afb97f29b5459d683a876eb1870364304738
Julia Stolyarova <yuliya.stolyarova@r1.team>
2026-03-01 19:54:51 +0700

    test

--- desktopApp/src/main/kotlin/com/example/deepseekchat/desktop/Main.kt
+++ desktopApp/src/main/kotlin/com/example/deepseekchat/desktop/Main.kt
@@ -1,5 +1,9 @@
 package com.example.deepseekchat.desktop
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.example.deepseekchat.shared.DeepSeekChatApp
@@ -10,5 +14,7 @@ fun main() = application {
         title = "DeepSeekChatApp"
     ) {
         DeepSeekChatApp()
+
+        Spacer(modifier = Modifier.padding(2.dp))
     }
 }
